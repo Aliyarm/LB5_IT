dc4320bbbf34cd93d6dc8ff7cecf5017831c9b84
Алияр Молдабеков <aliyar.m03@gmail.com>
2021-12-16 23:15:56 +0700

    Всё!

--- Program.cs
+++ Program.cs
@@ -6,64 +6,53 @@ namespace LB5_IT
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите длину последовтаельности.");
+            // ввод последовательности
+            Console.WriteLine("Введите длину последовтаельности, n.");
             int n = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Введите номер изменяемого члена последовательности.");
+            Console.WriteLine("Введите номер изменяемого члена последовательности, k.");
             int k = Convert.ToInt32(Console.ReadLine());
             while (k > n)
             {
                 Console.WriteLine("k должно быть меньше или равно n. Введите k ещё раз.");
                 k = Convert.ToInt32(Console.ReadLine());
             }
-            Console.WriteLine("Введите значение, на которое будет изменяться член последовательности.");
+            Console.WriteLine("Введите значение p, на которое будет изменяться член последовательности.");
             double p = Convert.ToDouble(Console.ReadLine());
-
+            // сортировка и вывод последовательности
             double[] array = new double[n];
             Random rand = new();
             for (int i = 0; i < array.Length; i++)
             {
                 array[i] = rand.Next(-100, 100);
             }
- 
-            double temp;
-            for (int i = 0; i < array.Length - 1; i++)
-            {
-                for (int j = i + 1; j < array.Length; j++)
-                {
-                    if (array[i] > array[j])
-                    {
-                        temp = array[i];
-                        array[i] = array[j];
-                        array[j] = temp;
-                    }
-                }
-            }
-
+            Array.Sort(array);
             Console.WriteLine("Изначальная последовательность:");
             foreach (int i in array)
             {
                 Console.Write(i + " ");
             }
             Console.WriteLine();
-
+            // вставляем p вместо a[k]
             array[k - 1] = p;
-            double x;
-            int j;
-            for (int i = 1; i < array.Length; i++)
+
+            // сортировка методом выбора по возрастанию с поиском минимума
+            for (int i = 0; i < array.Length; i++)
             {
-                x = array[i];
-                j = i;
-                while (j > 0 && array[j - 1] > x)
+                double min = array[i];
+                int minId = i;
+                for (int j = i + 1; j < array.Length; j++)
                 {
-                    temp = array[i];
-                    array[i] = array[j];
-                    array[j] = temp;
-                    j -= 1;
+                    if (array[j] < min)
+                    {
+                        min = array[j];
+                        minId = j;
+                    }
                 }
-                array[j] = x;
+                // замена
+                double temp = array[i];
+                array[i] = min;
+                array[minId] = temp;
             }
-
-
             foreach (double i in array)
             {
                 Console.Write(i + " ");
